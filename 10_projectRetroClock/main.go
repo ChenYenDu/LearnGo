719fb24c5c545bd126174d852c12e670b31883ce
ChenYenDu <chun.yen.tu@gmail.com>
2021-04-26 23:17:41 +0800

    add ticker to the terminal clock

--- 10_projectRetroClock/main.go
+++ 10_projectRetroClock/main.go
@@ -6,91 +6,8 @@ import (
 )
 
 func main() {
-	type numbers = [5]string
-	zero := numbers{
-		"███",
-		"█ █",
-		"█ █",
-		"█ █",
-		"███",
-	}
-	one := numbers{
-		"██ ",
-		" █ ",
-		" █ ",
-		" █ ",
-		"███",
-	}
-	two := numbers{
-		"███",
-		"  █",
-		"███",
-		"█  ",
-		"███",
-	}
-	three := numbers{
-		"███",
-		"  █",
-		"███",
-		"  █",
-		"███",
-	}
-	four := numbers{
-		"█ █",
-		"█ █",
-		"███",
-		"  █",
-		"  █",
-	}
-	five := numbers{
-		"███",
-		"█  ",
-		"███",
-		"  █",
-		"███",
-	}
-	six := numbers{
-		"███",
-		"█  ",
-		"███",
-		"█ █",
-		"███",
-	}
-	seven := numbers{
-		"███",
-		"  █",
-		"  █",
-		"  █",
-		"  █",
-	}
-	eight := numbers{
-		"███",
-		"█ █",
-		"███",
-		"█ █",
-		"███",
-	}
-	nine := numbers{
-		"███",
-		"█ █",
-		"███",
-		"  █",
-		"███",
-	}
 
-	colon := numbers{
-		"   ",
-		" ░ ",
-		"   ",
-		" ░ ",
-		"   ",
-	}
-
-	digits := [...]numbers{
-		zero, one, two, three, four, five, six, seven, eight, nine,
-	}
-
-	for {
+	for shift := 0; ; shift++ {
 
 		fmt.Print("\033[2J")
 		fmt.Print("\033[H")
@@ -99,7 +16,7 @@ func main() {
 
 		fmt.Printf("hour: %d, min: %d, sec: %d\n", hour, min, sec)
 
-		clock := [...]numbers{
+		clock := [...]placeholder{
 			digits[hour/10], digits[hour%10],
 			colon,
 			digits[min/10], digits[min%10],
@@ -107,15 +24,39 @@ func main() {
 			digits[sec/10], digits[sec%10],
 		}
 
+		// alarmed := sec%10 == 0
+
 		for line := range clock[0] {
-			for index, digit := range clock {
-				// colon blink
-				next := clock[index][line]
-				if digit == colon && sec%2 == 0 {
+			l := len(clock)
+
+			s, e := shift%l, l
+
+			if shift%(l*2) >= l {
+				s, e = 0, s
+			}
+
+			for j := 0; j < l-e; j++ {
+				fmt.Print("   ")
+			}
+
+			for i := s; i < e; i++ {
+				next := clock[i][line]
+
+				if clock[i] == colon && sec%2 == 0 {
 					next = "   "
 				}
+
 				fmt.Print(next, " ")
 			}
+
+			// for index, digit := range clock {
+			// 	// colon blink
+			// 	next := clock[index][line]
+			// 	if digit == colon && sec%2 == 0 {
+			// 		next = "   "
+			// 	}
+			// 	fmt.Print(next, " ")
+			// }
 			fmt.Println()
 		}
 
